5b2dfd751087caab173cc1d979fb3537ae6b9867
Kiyoshi Mizumoto <mizumoto@andgenie.jp>
2019-12-05 12:59:03 +0900

    orca: program updateを削除

--- orca/5.0.0-xenial/docker-entrypoint.sh
+++ orca/5.0.0-xenial/docker-entrypoint.sh
@@ -10,7 +10,7 @@ service supervisor start # for orca pusher
 
 # set ormaster password
 sudo -u orca psql orca -c "DELETE FROM tbl_passwd WHERE userid = 'ormaster';"
-NPASS=`md5pass $ORMASTER_PASS`
+NPASS=`md5pass $ORMASTER_PASSWORD`
 sudo -u orca psql orca -c "INSERT INTO tbl_passwd (userid, password) VALUES ('ormaster', '${NPASS}');"
 
 exec "$@"
