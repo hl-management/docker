707f51c9851902b363b7be3f4833c417a557c48b
Kiyoshi Mizumoto <mizumoto@andgenie.jp>
2020-01-10 11:53:20 +0900

    orca:5.1.0-bionic

---
+++ orca/5.1.0-bionic/README.md
@@ -0,0 +1,8 @@
+# orca docker
+
+## Summary
+
+日医標準レセプトソフト(ORCA)をDockerコンテナ上で実行します。
+
+## Docker HUB
+https://hub.docker.com/r/homis/orca
---
+++ orca/5.1.0-bionic/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -e
+
+# start services
+service postgresql start
+service rabbitmq-server start
+service jma-receipt start
+service supervisor start # for orca pusher
+
+# set ormaster password
+sudo -u orca psql orca -c "DELETE FROM tbl_passwd WHERE userid = 'ormaster';"
+NPASS=`md5pass $ORMASTER_PASSWORD`
+sudo -u orca psql orca -c "INSERT INTO tbl_passwd (userid, password) VALUES ('ormaster', '${NPASS}');"
+
+exec "$@"
